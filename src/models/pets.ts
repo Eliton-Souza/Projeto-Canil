8a484421e9aa709453ef80f84d17de8a1ad945c5
Eliton Souza <ess3@icomp.ufam.edu.br>
2023-04-12 11:44:13 -0400

    finalizando model

--- src/models/pets.ts
+++ src/models/pets.ts
@@ -1,5 +1,7 @@
+type PetType= 'dog' | 'cat' | 'fish';
+
 type Pet={
-    type: 'dog' | 'cat' | 'fish',
+    type: PetType,
     image: string,
     name: string,
     color: string,
@@ -134,5 +136,15 @@ const data: Pet[]= [
 export const Pet = {
     gettAll: (): Pet[]=>{
         return data;
+    },
+
+    getFromType: (type: PetType): Pet[] =>{
+        return data.filter(item => item.type===type);
+    },
+
+    getFromName: (name: string): Pet[] =>{
+        return data.filter(item =>
+            item.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+        );
     }
-}
+};
