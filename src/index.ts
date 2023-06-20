25fcd24253e5a4a11c35c6d546fd45b9f1365264
Eliton Souza <ess3@icomp.ufam.edu.br>
2023-06-20 18:25:52 -0400

    tirando 404

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,4 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(mainRouter);
 
-app.use((req: Request, res: Response)=>{
-    res.render("pages/404");
-})
-
 app.listen(port);
